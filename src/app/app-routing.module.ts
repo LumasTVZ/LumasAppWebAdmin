ea8c546a5a0f224fd5a5dcfe79a19c014e5770fc
DanijelCar <dcartvz@gmail.com>
2022-06-04 09:16:28 +0200

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', loadChildren: () => import('./pages/main-view/main-view.module').then(m => m.MainViewModule) },
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
 
 ];
 
